53dbaa026c112e03482c8499211650b6bc2c7e8c
charl0tte <karolinakozak9@gmail.com>
2019-01-13 21:46:17 +0100

    Add working uploading files

--- src/main/java/app/controller/UploadController.java
+++ src/main/java/app/controller/UploadController.java
@@ -1,4 +1,52 @@
 package app.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@RestController
+@Slf4j
+@CrossOrigin
+@RequestMapping("/upload/")
 public class UploadController {
+
+
+    @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("file") MultipartFile file) {
+
+        String name = "test.txt";
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = "C:/Projects/praca-dyplomowa";
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
+    }
+
+
 }
